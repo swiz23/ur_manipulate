1fed7a36d52476bcf441d4703a897210b2b2f6e9
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2019-05-23 18:38:16 -0500

    got ik solver to make end effector move in a horizontal line

--- src/ur5_node.cpp
+++ src/ur5_node.cpp
@@ -27,30 +27,32 @@ int main(int argc, char **argv)
     Eigen::VectorXd joint_angles(6);
     joint_angles << 0, 0.0, 0, 0, 0, 0;
     bool solution_found;
+    double x = 0.5;
     Eigen::MatrixXd Tsd(4,4);
     Eigen::MatrixXd end_effector(4,4);
-    Tsd << -1, 0, 0, 0.5,
-            0, 0, 1, 0.5,
-            0, 1, 0, 0.3,
-            0, 0, 0,   1;
-
-    solution_found = mr::IKinSpace(Slist, Mlist, Tsd, joint_angles, 0.01, 0.01);
-    std::cout << "Solution found? " << solution_found << std::endl;
-    end_effector = mr::FKinSpace(Mlist, Slist, joint_angles);
-    std::cout << "Joint angles\n" << joint_angles << std::endl;
-    std::cout << "MR FKinSpace position\n" << end_effector << std::endl;
-
-    joint_state.position[0] = joint_angles[0];
-    joint_state.position[1] = joint_angles[1];
-    joint_state.position[2] = joint_angles[2];
-    joint_state.position[3] = joint_angles[3];
-    joint_state.position[4] = joint_angles[4];
-    joint_state.position[5] = joint_angles[5];
 
     while(ros::ok())
     {
+        Tsd << -1, 0, 0,   x,
+                0, 0, 1, 0.5,
+                0, 1, 0, 0.3,
+                0, 0, 0, 1;
+        solution_found = mr::IKinSpace(Slist, Mlist, Tsd, joint_angles, 0.0005, 0.0005);
+        std::cout << "Solution found? " << solution_found << std::endl;
+        end_effector = mr::FKinSpace(Mlist, Slist, joint_angles);
+        std::cout << "Joint angles\n" << joint_angles << std::endl;
+        std::cout << "MR FKinSpace position\n" << end_effector << std::endl;
+
         joint_state.header.stamp = ros::Time::now();
+        joint_state.position[0] = joint_angles[0];
+        joint_state.position[1] = joint_angles[1];
+        joint_state.position[2] = joint_angles[2];
+        joint_state.position[3] = joint_angles[3];
+        joint_state.position[4] = joint_angles[4];
+        joint_state.position[5] = joint_angles[5];
         joint_pub.publish(joint_state);
+        x = x - 0.001;
+        std::cout << x << std::endl;
         loop_rate.sleep();
     }
 
